04dc970dc433d2d9fab425f22b2353e67ebc1022
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-11-01 20:33:31 -0700

    Added CORS to server.js for the API

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
+
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(apiPreface, require('./routes/area-route'));
 app.use(apiPreface, require('./routes/areas-route'));
 
