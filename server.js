f201d3dbfd9d57ae620f79ba31c63457db055c46
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-11-06 15:47:17 -0800

    enabled PUT and DELETE operations.

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 });
 
